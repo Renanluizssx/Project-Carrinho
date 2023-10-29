9b31a9e36266525ce72c9dc19e460c4bf96046a7
Renanluizssx <renanluizssx@gmail.com>
2023-10-29 05:52:49 -0300

    “mensagem”

---
+++ script/ecommerce.js
@@ -0,0 +1,45 @@
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key)) ?? [];
+const setLocalStorage = (key, item) =>
+  localStorage.setItem(key, JSON.stringify(item));
+  
+
+document.addEventListener("DOMContentLoaded", updateContent);
+
+function printProduct(product, index) {
+  const content = document.getElementById("content");
+
+  return content.innerHTML += `<div id="product${index}" class="product"> 
+    <div id="image${index}" class="image">
+     <img id="image-img${index}"
+      class="image-img" src="${product.image}">
+    </div>
+        <div id="product-info${index}"class="product-info">
+        
+        <h2 id="subtitle${index}" class="subtitle">${product.name}</h2> 
+        <span id="text${index}" class="text">R$ ${product.price}</span>
+        <button data-product-id="${index}" id="button${index}" class="button" onclick="saveProduct(${index})">ADICIONAR AO CARRINHO</button>
+        </div>
+    </div>`;
+}
+function updateContent() {
+  const products = getLocalStorage("products");
+
+  return products.forEach(printProduct);
+}
+function openIndex() {
+  const url = "index.html";
+  return window.open(url, "_self");
+}
+
+function saveProduct(index) {
+  console.log(index);
+  const products = getLocalStorage("products")[index];
+  return updateProduct(products);
+}
+
+function updateProduct(products) {
+  const productsProduct = getLocalStorage("carts");
+  setLocalStorage("carts", [ products, ...productsProduct]);
+  const url = "/pages/carrinho.html";
+  return window.open(url);
+}
