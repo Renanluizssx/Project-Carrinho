0ccc5f8753b8a7fb1fdbb68309f4b4036b945425
Renanluizssx <renanluizssx@gmail.com>
2024-01-01 10:43:28 -0300

    commitado

--- script/ecommerce.js
+++ script/ecommerce.js
@@ -1,14 +1,13 @@
 const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key)) ?? [];
 const setLocalStorage = (key, item) =>
   localStorage.setItem(key, JSON.stringify(item));
-  
 
 document.addEventListener("DOMContentLoaded", updateContent);
 
 function printProduct(product, index) {
   const content = document.getElementById("content");
 
-  return content.innerHTML += `<div id="product${index}" class="product"> 
+  return (content.innerHTML += `<div id="product${index}" class="product"> 
     <div id="image${index}" class="image">
      <img id="image-img${index}"
       class="image-img" src="${product.image}">
@@ -19,7 +18,7 @@ function printProduct(product, index) {
         <span id="text${index}" class="text">R$ ${product.price}</span>
         <button data-product-id="${index}" id="button${index}" class="button" onclick="saveProduct(${index})">ADICIONAR AO CARRINHO</button>
         </div>
-    </div>`;
+    </div>`);
 }
 function updateContent() {
   const products = getLocalStorage("products");
@@ -39,7 +38,7 @@ function saveProduct(index) {
 
 function updateProduct(products) {
   const productsProduct = getLocalStorage("carts");
-  setLocalStorage("carts", [ products, ...productsProduct]);
-  const url = "/pages/carrinho.html";
+  setLocalStorage("carts", [products, ...productsProduct]);
+  const url = "../pages/carrinho.html";
   return window.open(url);
 }
